e4ac4519886fad0a0867a2c9324701859f0e0abf
Dinazzz <dinazval@gmail.com>
2019-04-16 19:37:00 +0500

    Исправления после первой проверки

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import os
 import argparse
 from dotenv import load_dotenv
 
-load_dotenv()
-
 def make_short_link(token, url):
     url_bit_ly = 'https://api-ssl.bitly.com/v4/bitlinks'
     headers = {'Authorization': 'Bearer {}'.format(token)}
@@ -27,10 +25,7 @@ def check_link(token, url):
     url_bit_ly = 'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}'.format(bitlink=url)
     headers = {'Authorization': 'Bearer {}'.format(token)}
     response = requests.get(url_bit_ly, headers=headers)
-    if response.ok:
-        return 'Число переходов по ссылке {}'.format(count_clicks(token, url))
-    else:
-        return make_short_link(token, url)
+    return response.ok
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Аналитака ссылок bit.ly')
@@ -39,11 +34,15 @@ def parse_arguments():
     return args.url
 
 if __name__ == '__main__':
+    load_dotenv()
     url = parse_arguments()
     token = os.getenv('TOKEN')
     long_link = 'https://www.youtube.com/' # long link example
     short_link ='bit.ly/2WUrzNe' # short link example
     try:
-        print(check_link(token, url))     
+        if check_link(token, url):
+            print('Число переходов по ссылке {}'.format(count_clicks(token, url)))
+        else:
+            print(make_short_link(token, url))
     except requests.exceptions.HTTPError as http_error:
         print('The request failed', http_error, "Сheck the link is correct", sep='\n')
